80c49915ed8a643b105e983dc0d5fe901d29f264
lorenzozardo <lorenzozardo19@gmail.com>
2025-06-18 12:06:02 -0300

    Ajustes finais

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const toggle = document.querySelector('.mobileMenuToggle');
 const menu = document.querySelector('.navMenu');
 
 toggle.addEventListener('click', () => {
-    menu.classList.toggle('active');
-});
+    menu.classList.toggle('active')
+})
 
 // Fecha o menu quando clica em algum link
 document.querySelectorAll('.navLink').forEach(link => {
     link.addEventListener('click', () => {
-        menu.classList.remove('active');
-    });
+        menu.classList.remove('active')
+    })
 })
